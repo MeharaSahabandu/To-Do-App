54e55dff95ce73a866d19b1a7cde0a1d84f328c4
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-06-18 03:46:42 -0700

    material ui added

--- client/frontend/src/App.jsx
+++ client/frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import {Router, Routes, Route} from "react-router-dom";
+import header from "./components/header";
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<header/>}/>
+      </Routes>
+    </Router>
   )
 }
 
